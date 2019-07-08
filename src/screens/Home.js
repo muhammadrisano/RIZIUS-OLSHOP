1f9361101cc82afd91a3c1b03333eff1d21d7e23
zidni0192 <izedeledby@gmail.com>
2019-07-08 16:42:31 +0700

    change icon , title . modify navbar

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-
+import Navbar from '../components/Navbar'
 class Home extends Component {
 
 
     render() {
         return (
             <div>
-
+                <Navbar />
             </div>
         )
 
