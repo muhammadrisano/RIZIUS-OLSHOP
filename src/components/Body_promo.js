71474324860226d586fd8a8fa74e7436125695c0
Muhammad Risano Akbar <muhammadrisano@gmail.com>
2019-07-08 22:30:44 +0700

    tambahan body home

--- src/components/Body_promo.js
+++ src/components/Body_promo.js
@@ -1,77 +1,37 @@
 import React from 'react'
 import '../assets/css/home.css'
+import promo from '../database/promo'
 
 let Body_promo = () => {
 
+    const promoProduk = promo.map((produk, index) => {
+        return (
+            <div className="col-md-2">
+                <div class="card" key={index}>
+                    <img src={produk.url} class="card-img-top" alt="..." />
+                    <div class="card-body">
+                        <h5 class="card-title">{produk.title}</h5>
+                        <p class="card-text">{produk.subtitle}</p>
+                    </div>
+                </div>
+            </div>
+        )
+    })
+
     return (
+
         <div className="body_promo">
 
             <div className="container-fluid">
                 <div className="iklan">
+                    <div className="row">
 
+                    </div>
                 </div>
                 <div className="promo_terbaik">
+                    <h2>Promo Terbaik Untukmu</h2>
                     <div className="row">
-                        <div className="col-md-2">
-                            <div class="card">
-                                <img src="..." class="card-img-top" alt="..." />
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-2">
-                            <div class="card">
-                                <img src="..." class="card-img-top" alt="..." />
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-2">
-                            <div class="card">
-                                <img src="..." class="card-img-top" alt="..." />
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-2">
-                            <div class="card">
-                                <img src="..." class="card-img-top" alt="..." />
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-2">
-                            <div class="card">
-                                <img src="..." class="card-img-top" alt="..." />
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-2">
-                            <div class="card">
-                                <img src="..." class="card-img-top" alt="..." />
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-
-                                </div>
-                            </div>
-                        </div>
+                        {promoProduk}
                     </div>
                 </div>
             </div>
