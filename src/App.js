98d9f5602f0586a041dde0338588dce938702606
Khusni Jafar <khusnijafar@gmail.com>
2019-07-08 14:32:32 +0700

    footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import FooterPage from './components/Footer';
 
 
 class App extends Component {
@@ -6,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div>
-
+        <FooterPage />
       </div>
     )
   }
